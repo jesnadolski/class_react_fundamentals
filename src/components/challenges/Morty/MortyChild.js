4ab32ebabca66f17ade0e9975619828be7fddd31
jesnadolski <nadolski.jessica@gmail.com>
2021-02-04 20:05:36 -0500

    2/4/2021

---
+++ src/components/challenges/Morty/MortyChild.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+    Card, Button, CardImg, CardTitle, CardText, CardColumns,
+    CardSubtitle, CardBody
+  } from 'reactstrap';
+
+const MortyChild = (props) => {
+    return ( 
+        <Card>
+        <CardImg top width="100%" src={props.mortyCharacter.image} alt="Card image cap" />
+        <CardBody>
+          <CardTitle tag="h5">{props.mortyCharacter.name}</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">{props.mortyCharacter.species}</CardSubtitle>
+          <CardText>Status: {props.mortyCharacter.status}</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+
+     );
+}
+ 
+export default MortyChild;
